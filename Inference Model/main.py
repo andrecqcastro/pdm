6551f96ab81b46f5f7e27b1302db1cccf22fe92d
Pedro Rabelo Mendonça <pedrorabelo@discente.ufg.br>
2024-12-19 12:22:54 -0300

    Sentiment analysis inference model

---
+++ Inference Model/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from transformers import pipeline
+import os
+
+# Carregar modelo de análise de sentimento
+model_name = "EdwardSJ151/bert-amazon-reviews"
+sentiment_model = pipeline("text-classification", model=model_name)
+
+# Inicializar o aplicativo FastAPI
+app = FastAPI()
+
+# Definir o esquema de entrada
+class SentimentInput(BaseModel):
+    texts: List[str]  # Lista de textos para análise
+
+# Definir o esquema de saída
+class SentimentOutput(BaseModel):
+    label: str
+    score: float
+
+@app.post("/sentiment-analysis", response_model=List[SentimentOutput])
+async def analyze_sentiments(input_data: SentimentInput):
+    try:
+        # Fazer inferência para cada texto da entrada
+        predictions = [
+            {
+                "label": sentiment_model(text)[0]["label"],
+                "score": sentiment_model(text)[0]["score"]
+            }
+            for text in input_data.texts
+        ]
+
+        return predictions
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Erro na predição: {str(e)}")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Usa a porta do ambiente, padrão 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
